0ccf7edb25047c376a42ee2de0a6ea457b1d1344
mongilteacher <mongilteacher@gmail.com>
2024-05-07 14:35:11 +0900

    ArticleClass

---
+++ Assets/02.Scripts/Article.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System;
+
+public enum ArticleType
+{
+    Normal,
+    Notice,
+}
+// 게시글을 나타내는 데이터 클래스
+public class Article
+{
+    public ArticleType ArticleType; // 일반글? 공지사항글이냐?
+    public string Name;             // 글쓴이
+    public string Title;            // 글 제목
+    public string Content;          // 글 내용
+    public int Like;                // 좋아요 개수
+    public DateTime WriteTime;      // 글 쓴 날짜/시간
+}
