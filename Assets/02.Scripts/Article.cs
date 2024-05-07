9a2052fbd5f880529b56a784c04b5de4dd65f853
mongilteacher <mongilteacher@gmail.com>
2024-05-07 14:44:23 +0900

    커밋

--- Assets/02.Scripts/Article.cs
+++ Assets/02.Scripts/Article.cs
@@ -7,11 +7,10 @@ public enum ArticleType
     Notice,
 }
 // 게시글을 나타내는 데이터 클래스
-public class Article
+public class Article // Quest, Item, Achievement, Attendance 
 {
     public ArticleType ArticleType; // 일반글? 공지사항글이냐?
     public string Name;             // 글쓴이
-    public string Title;            // 글 제목
     public string Content;          // 글 내용
     public int Like;                // 좋아요 개수
     public DateTime WriteTime;      // 글 쓴 날짜/시간
--- Assets/02.Scripts/UI_Article.cs
+++ Assets/02.Scripts/UI_Article.cs
@@ -3,16 +3,15 @@ using UnityEngine.UI;
 // Article 데이터를 보여주는 게임 오브젝트
 public class UI_Article : MonoBehaviour
 {
-    public Text NameTextUI;        // 글쓴이
-    public Text TitleTextUI;      // 글 제목
-    public Text ContentTextUI;    // 글 내용
-    public Text LikeTextUI;       // 좋아요 개수
-    public Text WriteTimeUI;      // 글 쓴 날짜/시간
+    public Image ProfileImageUI;   // 프로필 이미지
+    public Text  NameTextUI;       // 글쓴이
+    public Text  ContentTextUI;    // 글 내용
+    public Text  LikeTextUI;       // 좋아요 개수
+    public Text  WriteTimeUI;      // 글 쓴 날짜/시간
     
     public void Init(Article article)
     {
         NameTextUI.text    = article.Name;
-        TitleTextUI.text   = article.Title;
         ContentTextUI.text = article.Content;
         LikeTextUI.text    = $"{article.Like}";
         WriteTimeUI.text   = $"{article.WriteTime}";
