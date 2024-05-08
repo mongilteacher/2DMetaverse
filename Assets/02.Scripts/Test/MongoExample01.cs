2bbb8688e4a5fc926c98d9bf520436c0f324fe07
mongilteacher <mongilteacher@gmail.com>
2024-05-08 12:14:22 +0900

    var

--- Assets/02.Scripts/Test/MongoExample01.cs
+++ Assets/02.Scripts/Test/MongoExample01.cs
@@ -8,6 +8,20 @@ public class MongoExample01 : MonoBehaviour
 {
     private void Start()
     {
+        // var: 암시적 타입으로써 데이터의 자료형을 자동으로 설정하는 키워드
+        // r-value로부터 자동으로 타입을 유추
+        // 장점: 간단하다.
+        // 단점: 자료형이 명확하지 않아서 휴먼 에러가 날 수 있다.
+        // 언제쓰면 좋냐?: 1.자료형이 너무 길 경우
+        //               2.foreach 반복문에서 명확할 경우
+        List<Article> articles = new List<Article>();
+        foreach (Article article in articles)
+        {
+            
+        }
+        
+        
+        
         // 몽고 데이터베이스에 연결
         // 연결 문자열: 데이터베이스 연결을 위한 정보가 담겨있는 문자열
         string connectionString = "mongodb+srv://mongil:mongil@cluster0.ixxxv9w.mongodb.net/";
